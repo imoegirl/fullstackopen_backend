1783a91d7b6b5ffe204395410a13b0de600763a8
imoegirl <imoegirl@outlook.com>
2021-01-15 14:38:13 +0800

    添加用户相关的代码

--- agent.js
+++
@@ -1,7 +0,0 @@
-console.log("i'm agent")
-
-const func = ()=>{
-    console.log('hahahah')
-}
-
-module.exports = func
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,58 +1,55 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+notesRouter.get('/', async (request, response) => {
+    const notes = await Note.find({}).populate('user', {username: 1, name: 1})
+    response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if(note) {
-                response.json(note)
-            }else{
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+    const note = await Note.findById(request.params.id)
+    if(note){
+        response.json(note)
+    }else{
+        response.status(404).end()
+    }
 })
 
-notesRouter.post('/',(request,response, next) => {
+notesRouter.post('/', async (request,response, next) => {
     const body = request.body
+    console.log("Body: ", request.body)
+    const user = await User.findById(body.userId)
+    console.log('UserData: ', user, "userId: ", body.userId)
+
     const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date()
+        date: new Date(),
+        user: user._id
     })
-    note.save()
-        .then(saveNote => {
-            response.json(saveNote)
-        })
-        .catch(error => next(error))
+
+    const saveNote = await note.save()
+    user.notes = user.notes.concat(saveNote._id)
+    await user.save()
+
+    response.json(saveNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(()=>{
-            response.status(204).end()
-        })
-        .catch(error=>next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response, next) => {
     const body = request.body
     const note = {
         content: body.content,
         important: body.important
     }
 
-    Note.findByIdAndUpdate(request.params.id, note, {new: true})
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
+    const updateNote = await Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    response.json(updateNote)
 })
 
 module.exports = notesRouter
